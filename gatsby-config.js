58bbcca855c257ed4a92c53cf7ce50479f54f2bf
Dustin Schau <dustinschau@gmail.com>
2022-09-28 11:27:10 -0700

    chore: update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   siteMetadata: {
     title: `basic-auth-headers`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-gatsby-cloud',
+      options: {
+        headers: {
+          "/*": [
+            `Basic-Auth: ${process.env.BASIC_AUTH_USER}:${process.env.BASIC_AUTH_PASSWORD}`
+          ]
+        },
+      }
+    }
+  ],
 }
